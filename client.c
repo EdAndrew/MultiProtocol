d156fdc9a94b0ac17704ed61b2a000a65d9af78f
ed <qiujia145@163.com>
2016-06-14 23:27:12 +0800

    #2

---
+++ client.c
@@ -0,0 +1,32 @@
+#include "wrap.h"
+
+void dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen);
+
+int main(int argc, int *argv[])
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8001);
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+    dg_cli(stdin, sockfd, (SA *) &servaddr, sizeof(servaddr));
+}
+
+void dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
+{
+	int	n;
+	char	sendline[MAXLINE], recvline[MAXLINE + 1], recvline1[MAXLINE + 1];
+
+	fgets(sendline, MAXLINE, fp);
+	sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+	n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+	recvline[n] = 0;	/* null terminate */
+    n = recvfrom(sockfd, recvline1, MAXLINE, 0, NULL, NULL);
+    recvline1[n] = 0;
+	fputs(recvline, stdout);
+    fputs(recvline1, stdout);
+}
--- main.c
+++ main.c
@@ -2,32 +2,65 @@
 
 int main(int argc, char **argv)
 {
-	int listenfd, connfd, port, clientlen;
-	struct sockaddr_in clientaddr;
-	clientlen = sizeof(clientaddr);	
+	int					listenfdTcp, connfd, listenfdUdp;
+	int					maxfdp1;
+	pid_t				childpid;
+	socklen_t			clilen;
+	struct sockaddr_in	cliaddr, servaddrTcp, servaddrUdp;
+	void				sig_chld(int);
+	fd_set				rset;
 
-	if (argc != 2)
-	{
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(1);
+	listenfdTcp = socket(AF_INET, SOCK_STREAM, 0);
+	listenfdUdp = socket(AF_INET, SOCK_DGRAM, 0);
+
+	bzero(&servaddrTcp, sizeof(servaddrTcp));
+	servaddrTcp.sin_family      = AF_INET;
+	servaddrTcp.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddrTcp.sin_port        = htons(8000);
+
+	bzero(&servaddrUdp, sizeof(servaddrUdp));
+	servaddrUdp.sin_family      = AF_INET;
+	servaddrUdp.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddrUdp.sin_port        = htons(8001);
+
+	bind(listenfdTcp, (SA *) &servaddrTcp, sizeof(servaddrTcp));
+	bind(listenfdUdp, (SA *) &servaddrUdp, sizeof(servaddrUdp));
+
+	listen(listenfdTcp, LISTENQ);
+	Signal(SIGCHLD, sig_chld);	/* must call waitpid() */
+	
+	for ( ; ; ) {
+		clilen = sizeof(cliaddr);
+		FD_ZERO(&rset);
+		FD_SET(listenfdTcp, &rset);
+		FD_SET(listenfdUdp, &rset);
+		maxfdp1 = max(listenfdTcp, listenfdUdp) + 1;
+		select(maxfdp1, &rset, NULL, NULL, NULL);
+
+		if (FD_ISSET(listenfdTcp, &rset))
+		{
+			if ( (connfd = accept(listenfdTcp, (SA *) &cliaddr, &clilen)) < 0)
+			{
+				if (errno == EINTR)
+					continue;		/* back to for() */
+				else
+					fprintf(stderr, "accept error");
+			}
+			if ( (childpid = fork()) == 0) /* child process */
+			{
+				close(listenfdTcp);	/* close listening socket */
+				str_echo_tcp(connfd);	/* process the request */
+				exit(0);
+			}
+			close(connfd);			/* parent closes connected socket */
+		}
+		else if (FD_ISSET(listenfdUdp, &rset))
+		{
+			if ( (childpid = fork()) == 0) /* child process */
+			{
+				str_echo_udp(listenfdUdp);	/* process the request */
+				exit(0);
+			}
+		}
 	}
-
-	port = atoi(argv[1]);
-	listenfd = Open_listenfd(port);
-	for(;;)
-	{
-		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-		doit(connfd);
-		close(connfd);
-	}
-}
-
-int doit(int fd)
-{
-	char buf[MAXLINE];
-	printf("Connect from TCP...\n");
-	Rio_readinitb(&rio, fd);
-	Rio_readlineb(&rio, buf, MAXLINE);
-	Rio_writen(fd, "Sent from TCP server\n", strlen("Sent from TCP server\n"));
-	Close(fd);
 }
